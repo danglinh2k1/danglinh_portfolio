42037f769977a32e4daf45266671c3122feca964
danglinh2k1 <danglinh106201@gmail.com>
2023-02-21 14:48:53 +0700

    mychange

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -6,7 +6,6 @@ const Skills = () => {
     <motion.div
     initial="hidden"
           whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
           transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: 50 },
