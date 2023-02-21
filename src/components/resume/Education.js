63d65115301478ccb9d2eb972bd7c5022db88c55
danglinh2k1 <danglinh106201@gmail.com>
2023-02-21 14:59:23 +0700

    my-change

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -14,10 +14,10 @@ const Education = () => {
       className=""
     >
       {/* part one */}
-      <div className="mt-12">
+      
         <section className="dark:bg-gray-800 dark:text-gray-100 rounded-xl">
           <div className="container max-w-5xl px-4 py-12 mx-auto">
-            <div className="grid gap-4 mx-4 sm:grid-cols-12">
+            <div className="flex flex-wrap gap-2 mx-4 sm:grid-cols-12">
               <div className="col-span-12 sm:col-span-3">
                 <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-violet-400">
                   <h3 className="text-3xl font-semibold">HaNoi university </h3>
@@ -45,7 +45,7 @@ const Education = () => {
             </div>
           </div>
         </section>
-      </div>
+    
     </motion.div>
   );
 };
